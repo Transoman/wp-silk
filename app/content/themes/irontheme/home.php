d92d8d1b7e04803f943ba7648b333a1c44671069
transoman <vasiag10@gmail.com>
2019-12-16 17:09:07 +0200

    remove favicon inline

--- app/content/themes/irontheme/home.php
+++ app/content/themes/irontheme/home.php
@@ -4,17 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/favicon/site.webmanifest">
-  <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg" color="#000">
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon.ico">
-  <meta name="msapplication-TileColor" content="#000">
-  <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/img/favicon/browserconfig.xml">
   <meta name="theme-color" content="#000">
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 <body id="top" <?php body_class(); ?>>
 
